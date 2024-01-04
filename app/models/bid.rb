479071ddf281da472b693646b640a8f4c2e7f7e8
ugorji eke <ugorjiekechibuzo@gmail.com>
2024-01-04 15:09:43 +0100

    validation done on bidding

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,3 +1,21 @@
 class Bid < ApplicationRecord
   belongs_to :car
+
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :amount_greater_than_default_bid
+  validates :closing_time_in_future
+
+  private
+
+  def amount_greater_than_default_bid
+    if amount.present? && amount <= car.default_bid
+      errors.add(:amount, "must be greater than the default bid")
+    end
+  end
+
+  def closing_time_in_future
+    if closing_time.present? && closing_time <= Time.now
+      errors.add(:closing_time, "must be in the future")
+    end
+  end
 end
